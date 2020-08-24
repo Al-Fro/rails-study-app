be568cb928780f0005578c1318a244536e87a799
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-08-24 13:02:06 +0300

    add tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  def setup
+    @user = users(:one)
+  end
+
+  test 'should get new' do
+    get new_session_path
+    assert_response :success
+  end
+
+  test 'should get create' do
+    post session_path, params: { session: { email: @user.email, password: 'secret' } }
+    assert_response :redirect
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:one)
+  end
+
   test 'should get new' do
-    get :new
+    get new_user_path
     assert_response :success
+  end
+
+  test 'should get create' do
+    post users_path, params: { user: { name: @user.name,  
+                                       email: @user.email, 
+                                       password: 'secret', 
+                                       password_confirmation: 'secret' } }
+    assert_response :redirect
   end
 end
