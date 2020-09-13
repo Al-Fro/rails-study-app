be50491ad9494a34b0cf8af6b90110038cf82aa3
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-09-13 19:32:00 +0300

    docker volumes, update webpacker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def print_bootstrap_alerts
     flash.each do |message_type, message|
       concat(content_tag(:div, message, class: ["alert", "alert-#{message_type}", "flash-message"]))
-      return
     end
+    return
   end
 end
